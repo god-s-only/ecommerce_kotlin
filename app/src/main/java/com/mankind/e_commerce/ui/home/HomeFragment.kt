f0ea768b2dcaf3771a7afec55847836d0ecf1cfc
unknown <joshxchris098@gmail.com>
2025-03-12 06:47:24 -0700

    almost completed the home fragment

--- app/src/main/java/com/mankind/e_commerce/ui/home/HomeFragment.kt
+++ app/src/main/java/com/mankind/e_commerce/ui/home/HomeFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
@@ -12,8 +13,8 @@ import com.mankind.e_commerce.R
 import com.mankind.e_commerce.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
-    lateinit var binding:FragmentHomeBinding
-
+    private lateinit var binding:FragmentHomeBinding
+    private val locations = arrayOf("USA", "Germany", "Nigeria")
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -23,9 +24,8 @@ class HomeFragment : Fragment() {
             R.layout.fragment_home,
             container,
             false)
-
+        val arrayAdapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, locations)
+        binding.spinner.adapter = arrayAdapter
         return binding.root
     }
-
-
 }
